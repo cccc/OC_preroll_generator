c1eccecf0a70849e3c60691163219820046547cf
Florian Zeitz <florob@babelmonkeys.de>
2017-04-25 19:25:49 +0200

    Add .ts generation

--- gen.sh
+++ gen.sh
@@ -6,11 +6,17 @@ fi
 
 OUTSVG="$1.svg"
 OUTPNG="$1.png"
+OUTTS="$1.ts"
 
 sed -e s/\$id/"$1"/ -e s/\$personnames/"$2"/ -e s/\$title/"$3"/ preroll.svg > "$OUTSVG" \
 && inkscape -e "$OUTPNG" "$OUTSVG"
 rm -f "$OUTSVG"
 
-if [ ! -f postroll.png ]; then
+ffmpeg -loop 1 -i "$OUTPNG" -ar 48000 -ac 1 -f s16le -i /dev/zero -ar 48000 -ac 1 -f s16le -i /dev/zero -map 0:0 -c:v mpeg2video -q:v 0 -aspect 16:9 -map 1:0 -map 2:0 -t 5 -f mpegts "$OUTTS"
+rm -f "$OUTPNG"
+
+if [ ! -f postroll.ts ]; then
 	inkscape -e "postroll.png" "postroll.svg"
+	ffmpeg -loop 1 -i "postroll.png" -ar 48000 -ac 1 -f s16le -i /dev/zero -ar 48000 -ac 1 -f s16le -i /dev/zero -map 0:0 -c:v mpeg2video -q:v 0 -aspect 16:9 -map 1:0 -map 2:0 -t 5 -f mpegts "postroll.ts"
+	rm -f postroll.png
 fi
